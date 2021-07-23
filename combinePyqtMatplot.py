456d2645b0e7f8d213465f6c7ed4f2e964a700e9
108360224 <56162067+108360224@users.noreply.github.com>
2021-07-23 15:45:49 +0800

    Update combinePyqtMatplot.py

--- combinePyqtMatplot.py
+++ combinePyqtMatplot.py
@@ -4,6 +4,8 @@ import sys
 import time
 
 import numpy as np
+import pandas
+import pandas as pd
 from PyQt5 import uic, QtCore, QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QMessageBox, QWidget, \
     QPushButton, QSizeGrip, QHBoxLayout, QListWidget, QCheckBox, QListWidgetItem, QStyleOption, QStyle, QLabel
@@ -14,14 +16,22 @@ from matplotlib.backends.backend_qt5 import NavigationToolbar2QT as NavigationTo
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 from PyQt5 import QtWidgets
+import seaborn as sns
 import random
 from numpy.random import randn
 #from pyqt5_plugins.examplebutton import QtWidgets
+
 from DragAndDrop import DropWidget,Button
+
 widgetForm, baseClass = uic.loadUiType("plot.ui")
 from functools import partial
-
 class PlotWindow(widgetForm, baseClass):
+    SCATTER = 1
+    BOX_PLOT = 2
+    BOX_GROUP_PLOT = 3
+    GROUP_PLOT = 4
+    HISTOGRAM_PLOT = 5
+    PLOT = 6
     def __init__(self):
         super(baseClass, self).__init__()
         self.setupUi(self)
@@ -62,11 +72,90 @@ class PlotWindow(widgetForm, baseClass):
         self.ListLayout.addWidget(itemWidget)
 
         self.plotFuncDict = {}
-        self.funcList = []
+        self.plotParameterDict = {}
+
         self.currentColor = list(np.random.choice(range(256), size=3))
         self.currentSetItem=[]
+    def runPlotFunction(self,funcname,df):
 
-    def runPlotFunction(self,func, *args, setYourLabel=None, **kwargs):
+        if funcname == self.SCATTER:
+
+            if len(df.columns) < 2:
+                print("warning select 2 columns")
+                return
+            try:
+                val = int(df[df.columns[0]].values[0])
+            except ValueError:
+                print("select only number")
+                return
+            colors = ['#FF0000', '#00FF00', '#0000FF', '#F00000', '#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd',
+                      '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+            mStyles = ["*", "+", "x", ".", ",", "o", "v", "^", "<", ">", "1", "2", "3", "4", "8", "s", "p", "P", "h",
+                       "H",
+                       "X", "D", "d", "|", "_", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+                       ]
+            if len(df.columns) >= 2:
+                for i in range(1, len(df.columns)):
+                    # plt.scatter(df[selectedCols[0]], df[selectedCols[i]], c=colors[i], marker=mStyles[i], label=selectedCols[i])
+                    self.addPlotFunction(plt.scatter, x=df[df.columns[0]], y=df[df.columns[i]],
+                                                    c=colors[i], marker=mStyles[i], label=str(df.columns[i]))
+                    d = {df.columns[0]:df[df.columns[0]],df.columns[i]:df[df.columns[i]]}
+                    self.plotParameterDict[str(df.columns[i])]={'funcname':funcname,'df':pd.DataFrame(d)}
+
+        if funcname == self.BOX_PLOT:
+            self.plotParameterDict['BOX'] = {'funcname': funcname, 'df': df}
+
+
+            self.addPlotFunction(sns.boxplot,data=df,setYourLabel='BOX')
+
+        if funcname == self.BOX_GROUP_PLOT:
+            if len(df.columns) < 2:
+                print("warning select 2 columns")
+                return
+            self.plotParameterDict[str(df.columns[0])] = {'funcname': funcname, 'df': df}
+
+
+            self.addPlotFunction(sns.boxplot,x=df.columns[0],y=df.columns[1],data=df,setYourLabel=df.columns[0])
+
+        if funcname == self.GROUP_PLOT:
+            if len(df.columns) < 3:
+                print("warning select 3 columns")
+                return
+            df = df.sort_values(by=df.columns[0])
+            dfgb = df.groupby(df.columns[0])
+            dfgb_dict = dfgb.indices
+            for key, group in dfgb_dict.items():
+                print(key)
+                X = df[df.columns[1]][group].values
+                Y = df[df.columns[2]][group].values
+                idx = list(dfgb_dict.keys()).index(key)
+                # plt.scatter(X,Y,label=str(key))
+                self.addPlotFunction(plt.scatter, x=X, y=Y, label=str(key))
+                d = {df.columns[0]:key,df.columns[1]:X,df.columns[2]:Y}
+                self.plotParameterDict[key] = {'funcname': funcname, 'df': pd.DataFrame(d)}
+        if funcname == self.HISTOGRAM_PLOT:
+            if len(df.columns) > 1:
+                print("select only one column")
+                return
+            try:
+                val = int(df[df.columns[0]].values[0])
+            except ValueError:
+                print("select only number")
+                return
+            self.plotParameterDict[str(df.columns[0])] = {'funcname': funcname, 'df': df}
+            self.addPlotFunction(sns.distplot,a=df[df.columns[0]], hist=True, kde=True, rug=False, bins=int(180/5),
+                     color='darkblue',hist_kws={'edgecolor':'black'},
+                     kde_kws={'linewidth': 3},
+                     rug_kws={'color': 'black'},label=df.columns[0])
+        if funcname == self.PLOT:
+
+            for col in df.columns:
+                d={col:df[col]}
+                self.plotParameterDict[col] = {'funcname': funcname, 'df':pd.DataFrame(d)}
+                self.addPlotFunction(plt.plot,df[col],label=col)
+    def addPlotFunction(self,func, *args, setYourLabel=None, **kwargs):
+
+
         if not setYourLabel == None:
             if not str(setYourLabel) in self.plotFuncDict:
                 self.setItem([str(setYourLabel)])
@@ -82,45 +171,74 @@ class PlotWindow(widgetForm, baseClass):
                 self.plotFuncDict[str(kwargs['label'])] = {'func': func, 'args': args, 'kwargs': kwargs}
 
         else:
-            self.funcList.append({'func': func, 'args':args, 'kwargs':kwargs})
+            pass
         #print(self.plotFuncDict)
 
     def onLayoutChange(self):
         self.plotCanvas.fig.clf()
 
 
-        if self.secAxisLayout.count() > 0:
+        if self.secAxisLayout.count() > 0 and self.fstAxisLayout.count() > 0:
             self.plotCanvas.isOnlyOneAxes = False
             plt.axes(self.plotCanvas.axes121)
             for i in range(self.fstAxisLayout.count()):
                 key = self.fstAxisLayout.itemAt(i).widget().text()
                 # print("key",key)
-                item = self.currentPlotFuncDict[str(key)]
+                item = self.plotFuncDict[str(key)]
                 item['func'](*item['args'], **item['kwargs'])
-            for f in self.funcList:
-                f['func'](*f['args'], **f['kwargs'])
+            plt.legend(loc='upper left')
             plt.axes(self.plotCanvas.axes122)
             for i in range(self.secAxisLayout.count()):
                 key = self.secAxisLayout.itemAt(i).widget().text()
                 #print("key",key)
-                item = self.currentPlotFuncDict[str(key)]
+                item = self.plotFuncDict[str(key)]
                 item['func'](*item['args'],**item['kwargs'])
-            for f in self.funcList:
-                f['func'](*f['args'], **f['kwargs'])
+            plt.legend(loc='upper left')
         else:
             self.plotCanvas.isOnlyOneAxes = True
             plt.axes(self.plotCanvas.axes111)
             for i in range(self.fstAxisLayout.count()):
                 key = self.fstAxisLayout.itemAt(i).widget().text()
                 # print("key",key)
-                item = self.currentPlotFuncDict[str(key)]
+                item = self.plotFuncDict[str(key)]
                 item['func'](*item['args'], **item['kwargs'])
-            for f in self.funcList:
-                f['func'](*f['args'], **f['kwargs'])
+            for i in range(self.secAxisLayout.count()):
+                key = self.secAxisLayout.itemAt(i).widget().text()
+                # print("key",key)
+                item = self.plotFuncDict[str(key)]
+                item['func'](*item['args'], **item['kwargs'])
+            plt.legend(loc='upper left')
         self.plotCanvas.draw()
 
+    def utils_recognize_type(self,df, col, max_cat=20):
+        if (df[col].dtype == "O") | (df[col].nunique() < max_cat):
+            return "cat"
+        else:
+            return "num"
+    def creatDfFromTable(self,df):
+        df = df.dropna()
 
+        # print(df.head())
+        # fig, ax = plt.subplots(nrows=1, ncols=1, sharex=False, sharey=False)
+        # print(df.index)
+        selected = self.tableWidget.selectedItems()
+        selectedCols = []
+        selectedRows = []
+        if selected:
+            for item in selected:
+                if df.columns[item.column()] not in selectedCols:
+                    selectedCols.append(df.columns[item.column()])
+                if item.row() not in selectedRows:
+                    selectedRows.append(item.row())
+            df = df[selectedCols]
+            df = pd.DataFrame(df, index=selectedRows)
+        else:
+            for col in df.columns:
+                if self.utils_recognize_type(df, col) != 'cat':
+                    selectedCols.append(col)
+            df = df[selectedCols]
 
+        return df,selectedCols,selectedRows
     def setItem(self,list):
         for item in list:
             b = Button(str(item))
@@ -136,100 +254,55 @@ class PlotWindow(widgetForm, baseClass):
 
 
         self.plotCanvas.fig.clf()
-        selects = self.tableWidget.selectedItems()
-        if selects:
-            self.currentPlotFuncDict[str(key)]['kwargs']['x'] = np.zeros(int(len(selects)/2))
-            self.currentPlotFuncDict[str(key)]['kwargs']['y'] = np.zeros(int(len(selects)/2))
-            col0count=0
-            col1count=0
-            for select in selects:
-                row = select.row()
-                if select.column() == 0:
-                    self.currentPlotFuncDict[str(key)]['kwargs']['x'][col0count] = copy.deepcopy(self.plotFuncDict[str(key)]['kwargs']['x'][row])
-                    col0count = col0count + 1
-                if select.column() == 1:
-                    self.currentPlotFuncDict[str(key)]['kwargs']['y'][col1count] = copy.deepcopy(self.plotFuncDict[str(key)]['kwargs']['y'][row])
-                    col1count = col1count + 1
 
-            if not len(self.currentPlotFuncDict[str(key)]['kwargs']['x']) == len(self.currentPlotFuncDict[str(key)]['kwargs']['y']):
-                msg = QMessageBox()
-                msg.setText("X and Y must be the same size")
-                msg.setWindowTitle("hint")
-                msg.exec_()
-                return
+        selected = self.tableWidget.selectedItems()
+        if selected:
 
-            plt.axes(self.plotCanvas.axes121)
-            for i in range(self.fstAxisLayout.count()):
-                key = self.fstAxisLayout.itemAt(i).widget().text()
-                # print("key",key)
-                item = self.currentPlotFuncDict[str(key)]
-                item['func'](*item['args'], **item['kwargs'])
-            for f in self.funcList:
-                f['func'](*f['args'], **f['kwargs'])
+            df, c, r = self.creatDfFromTable(self.eternalplotParameterDict[str(key)]['df'])
+            print(df)
 
-            plt.axes(self.plotCanvas.axes122)
-            for i in range(self.secAxisLayout.count()):
-                key = self.secAxisLayout.itemAt(i).widget().text()
-                # print("key",key)
-                item = self.currentPlotFuncDict[str(key)]
-                item['func'](*item['args'], **item['kwargs'])
-            for f in self.funcList:
-                f['func'](*f['args'], **f['kwargs'])
 
-            self.plotCanvas.draw()
+            self.runPlotFunction(self.plotParameterDict[str(key)]['funcname'],df)
 
 
 
-        else:
-            item = self.plotFuncDict[str(key)]
+        df = self.eternalplotParameterDict[str(key)]['df']
+        print(df)
+        self.tableWidget.clear()
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setColumnCount(0)
+        self.tableWidget.setColumnCount(len(df.columns))
+        for i in range(len(df.columns)):
+            for j in range(len(df[df.columns[i]])):
+                print(df[df.columns[i]][j])
+                if i == 0:
+                    self.tableWidget.insertRow(self.tableWidget.rowCount())
+                self.tableWidget.setItem(j, i, QtWidgets.QTableWidgetItem(str(df[df.columns[i]][j])))
 
-            if 'y' in item['kwargs']:
-                ydata = item['kwargs']['y']
-            if 'x' in item['kwargs']:
-                xdata = item['kwargs']['x']
-            try:
-                print('args ', item['args'])
-                data = item['args'][0]
-            except:
-                pass
+        self.tableWidget.setHorizontalHeaderLabels(df.columns)
 
-            self.tableWidget.clear()
-            self.tableWidget.setRowCount(0)
-            self.tableWidget.setColumnCount(0)
-
-            for i in range(len(xdata)):
-                self.tableWidget.insertRow(self.tableWidget.rowCount())
-                self.tableWidget.setColumnCount(2)
-                self.tableWidget.setItem(self.tableWidget.rowCount() - 1, 0, QtWidgets.QTableWidgetItem(str(xdata[i])))
-                self.tableWidget.setItem(self.tableWidget.rowCount() - 1, 1, QtWidgets.QTableWidgetItem(str(ydata[i])))
-
-        '''
-        box = QCheckBox(str(i))
-        item = QListWidgetItem()
-        self.ValuelistWidget.addItem(item)
-        #self.ValueListWidget.addItem(str(i))
-        box.stateChanged.connect(self.filterCheckBoxChangeState)
-        self.ValuelistWidget.setItemWidget(item, box)
-        '''
+        self.onLayoutChange()
 
 
 
     def clearPlot(self):
         self.plotCanvas.fig.clf()
         self.plotFuncDict = {}
-        self.funcList = []
+        self.plotParameterDict={}
+        self.eternalplotParameterDict={}
         self.tableWidget.clear()
-        for i in range(self.fstAxisLayout.count()):
+        for i in range(self.fstAxisLayout.count()-1,-1,-1):
             self.fstAxisLayout.itemAt(i).widget().setParent(None)
-        for i in range(self.itemLayout.count()):
+        for i in range(self.itemLayout.count()-1,-1,-1):
             self.itemLayout.itemAt(i).widget().setParent(None)
-        for i in range(self.secAxisLayout.count()):
+        for i in range(self.secAxisLayout.count()-1,-1,-1):
             self.secAxisLayout.itemAt(i).widget().setParent(None)
 
 
     def show(self):
         super().show()
-        self.currentPlotFuncDict = copy.deepcopy(self.plotFuncDict)
+        self.eternalplotParameterDict = copy.deepcopy(self.plotParameterDict)
+        self.tableWidget.clear()
 
 
 
@@ -252,7 +325,6 @@ class PlotWindow(widgetForm, baseClass):
         # do stuff
         self.clearPlot()
         event.accept()  # let the window close
-
 
 
 
